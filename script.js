07ff42a241ba7c8bb75950929b7505e5a01f6133
IgorChechelnitskiy <chekorsar@gmail.com>
2023-05-19 13:40:12 +0300

    Add hover for buttons and fixed some bugs

--- script.js
+++ script.js
@@ -1,124 +1,70 @@
 
-    let slideIndex = 1;
-    /* Вызываем функцию, которая реализована ниже: */
-    showSlides(slideIndex);
+/*#region__________________________________________Hero-slider*/
 
-    /* Увеличиваем индекс на 1 — показываем следующий слайд: */
-    function nextSlide() {
-        showSlides(slideIndex += 1);
-    }
 
-    /* Уменьшаем индекс на 1 — показываем предыдущий слайд: */
-    function previousSlide() {
-        showSlides(slideIndex -= 1);
-    }
+let heroSlideIndex = 1;
 
-    /* Устанавливаем текущий слайд: */
-    function currentSlide(n) {
-        showSlides(slideIndex = n);
-    }
-
-    /* Функция перелистывания: */
-    function showSlides(n) {
-        /* Обращаемся к элементам с названием класса "item", то есть к картинкам: */
-        let slides = document.getElementsByClassName("item");
-
-        /* Проверяем количество слайдов: */
-        if (n > slides.length) {
-            slideIndex = 1
-        }
-        if (n < 1) {
-            slideIndex = slides.length
-        }
-
-        /* Проходим по каждому слайду в цикле for: */
-        for (let slide of slides) {
-            slide.style.display = "none";
-        }
-        /* Делаем элемент блочным: */
-        slides[slideIndex - 1].style.display = "block";
-    }
-//
-// _________________________________________________________________________________________________
-// let sliders = document.querySelectorAll('.slider');
-// // sliders - список всех элементов с классом class="slider"
-// // sliders[0] — первый элемент, sliders[1] — второй, sliders[i] — i-тый.
-//
-// for (let i = 0; i < sliders.length; i++) {
-//     // Перебирает все, вызывает функцию для каждого.
-//     init_slider(sliders[i]);
-// }
-//
-// function init_slider(slider) {
-//     // Значение slider: Очередной sliders[i], переданный при вызове функции.
-//
-//     let slide = slider.querySelectorAll('.item');
-//     // Вместо document.query... Получается список всех class="slide"
-//     // которые находятся где-то внутри текущего элемента slider.
-//
-//     let next = slider.querySelector('.next');
-//     // Кнопка next внутри этого slider.
-//
-//     let i = 0;
-//     // Номер текущего "открытого" слайда.
-//
-//     next.addEventListener('click', function () {
-//         slide[i].classList.remove('block');
-//         // slide[i] - открытый слайд. Скрываем.
-//
-//         i = (i + 1) % slide.length; // (*1)
-//
-//         slide[i].classList.add('block');
-//         // slide[i] - следующий слайд. Показываем.
-//     });
-// }
-
-// _________________________________________________________________________________________________
-
-let sliders = document.querySelectorAll('.slider');
-// sliders - список всех элементов с классом class="slider"
-// sliders[0] — первый элемент, sliders[1] — второй, sliders[i] — i-тый.
-
-for (let i = 0; i < sliders.length; i++) {
-    // Перебирает все, вызывает функцию для каждого.
-    init_slider(sliders[i]);
+function heroNextSlide() {
+    heroShowSlides(heroSlideIndex += 1);
 }
 
-function init_slider(slider) {
-    // Значение slider: Очередной sliders[i], переданный при вызове функции.
-
-    let slideIndex = 1;
-    showSlides(slideIndex);
-
-    function nextSlide() {
-        showSlides(slideIndex += 1);
-    }
-
-    function previousSlide() {
-        showSlides(slideIndex -= 1);
-    }
-
-    function currentSlide(n) {
-        showSlides(slideIndex = n);
-    }
-
-    function showSlides(n) {
-        let i;
-        const slides = document.getElementsByClassName("item");
-        const dots = document.getElementsByClassName("dot");
-        if (n > slides.length) {
-            slideIndex = 1
-        }
-        if (n < 1) {
-            slideIndex = slides.length
-        }
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace("active", "");
-        }
-        slides[slideIndex - 1].style.display = "block";
-        dots[slideIndex - 1].className += "active";
-    }
+function heroPreviousSlide() {
+    heroShowSlides(heroSlideIndex -= 1);
 }
+
+function heroShowSlides(n) {
+    let slides = document.getElementsByClassName("hero-item");
+
+    if (n > slides.length) {
+        heroSlideIndex = 1
+    }
+    if (n < 1) {
+        heroSlideIndex = slides.length
+    }
+
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+    slides[heroSlideIndex - 1].style.display = "block";
+}
+
+
+heroShowSlides(heroSlideIndex);
+
+/*#endregion*/
+
+/*#region__________________________________________Projects-slider*/
+
+let projectsSlideIndex = 1;
+
+function projectsNextSlide() {
+    projectsShowSlides(projectsSlideIndex += 1);
+}
+
+function projectsPreviousSlide() {
+    projectsShowSlides(projectsSlideIndex -= 1);
+}
+
+function projectsCurrentSlide(n) {
+    projectsShowSlides(projectsSlideIndex = n);
+}
+
+function projectsShowSlides(n) {
+    let slides = document.getElementsByClassName("projects-item");
+
+    if (n > slides.length) {
+        projectsSlideIndex = 1
+    }
+    if (n < 1) {
+        projectsSlideIndex = slides.length
+    }
+
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+    slides[projectsSlideIndex - 1].style.display = "flex";
+}
+
+projectsShowSlides(projectsSlideIndex);
+
+/*#endregion*/
